253640f33a50e56e576b679531d36b33a196b937
yepvaishz <poti.vaishnavi21@gmail.com>
2024-02-21 23:48:44 +0530

    added slider to homepage

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,33 +1,66 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import './Home.css'
+import Sec2_img1 from './img/Sec2_img1.jpg'
+import Sec2_img2 from './img/Sec2_img2.jpg'
+import Sec2_img3 from './img/Sec2_img3.jpg'
+import Slider from "./Slider";
 
 const Home = () => {
   return (
     <>
-      <div className="home-container">
-        <div className="hero-section">
-          <h1>Welcome to Our Travel Website</h1>
-          <p>Explore the world with us!</p>
-          <a href="/tours" className="btn btn-primary">
-            View Tours
-          </a>
-        </div>
-        <div className="featured-destinations">
-          <h2>Featured Destinations</h2>
-          <div className="destination">
-            <img src="./Home/img/bridge.jpg" alt="Destination 1" />
-            <h3>Destination 1</h3>
-            <p>Explore the wonders of Destination 1.</p>
+      <div>
+        <div className="main-body">
+          <div className="section1">
+
+            <div className="navbar">
+              <div className="choose-button">
+                <button>Choose for me</button>
+              </div>
+
+              <div className="sign-button">
+                <button>Sign In</button>
+              </div>
+            </div>
+            <div className="sec1-text">
+              Discover story-worthy travel moments
+            </div>
+
           </div>
-          <div className="destination">
-            <img src="./Home/img/sea.jpg" alt="Destination 2" />
-            <h3>Destination 2</h3>
-            <p>Experience the beauty of Destination 2.</p>
-          </div>
+
         </div>
+        <div className="section2">
+          <div className="button-text-sec2">
+            <div className="sec2-text">
+              PLAN YOUR TRIP
+              <span>Where to next?</span>
+
+            </div>
+            <div className="button-container-sec2">
+              <button>View all destinations</button>
+            </div>
+          </div>
+
+
+          <img src={Sec2_img1} alt="Road bordered by majestic pillars" />
+          <img src={Sec2_img2} alt="Goden gate bridge" />
+          <img src={Sec2_img3} alt="Acastle" />
+          
+        </div>
+
+        <Slider/>
+
+        <div className="hero-image">
+          <p>BEST IN TRAVEL 2024</p>
+          <button>Discover the winners</button>
+        </div>
+
       </div>
-    </>
-  );
+   
+
+
+      </>
+      );
 };
 
 export default Home;
