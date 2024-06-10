745ec56234c2b930f5dc25dcb4cd1af330b7ccef
yepvaishz <poti.vaishnavi21@gmail.com>
2024-06-10 17:51:35 +0530

    fixed certificatse

--- src/FooterStuff/Certificate/Certificate.jsx
+++ src/FooterStuff/Certificate/Certificate.jsx
@@ -17,16 +17,10 @@ const Certificate = () => {
 
   return (
     <>
-      <div
-        className="bg-img"
-        style={{
-          backgroundImage: `url(https://w0.peakpx.com/wallpaper/43/30/HD-wallpaper-travel-couple-lifestyle.jpg)`,
-          height: "100vh",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      ></div>
+      <div className="cert-bg-img">
+        <img src="https://w0.peakpx.com/wallpaper/43/30/HD-wallpaper-travel-couple-lifestyle.jpg" className="cert-bg-img" />
+
+      </div>
       <form onSubmit={handleSubmit}>
         <div className="border">
           <h3 className="header-text">Hey, Enter name</h3>
@@ -35,7 +29,7 @@ const Certificate = () => {
             onChange={handleChange}
             value={uname}
             className="input-field"
-          ></input>
+          />
           <button type="submit" className="submit-button">
             Submit
           </button>
